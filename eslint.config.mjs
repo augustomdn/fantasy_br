d358cb96cbb7113a3197ee478f1a0714d94aeb5a
Augusto Nascimento <augustomeirelesn@hotmail.com>
2025-12-11 20:48:01 -0300

    P-01: feat: Configurar Prettier e instalar dependências.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,26 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+// ----------------------------------------
+// 1. Importe a configuração do Prettier
+import prettierConfig from 'eslint-config-prettier';
+// ----------------------------------------
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  // ----------------------------------------
+  // 2. Adicione o Prettier como o último item
+  prettierConfig,
+  // ----------------------------------------
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+
+    'next-env.d.ts',
   ]),
 ]);
 
